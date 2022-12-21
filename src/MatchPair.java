4ae8515cdf726594b570eb15a2b4d26770bdcf0b
FatemaAlbalushi <fatoomsaid188@gmail.com>
2022-12-21 03:04:34 -0800

    Add match pair program

--- src/MatchPair.java
+++ src/MatchPair.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * This program will create array list of array that the submission of the two
@@ -15,13 +16,18 @@ public class MatchPair {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] arr = { 2, 3, 1, 8, 6, 3, 4 };
+		int[] arr = { 6, 7, 4, 3, 4, 5, 6 };
 		int target = 10;
 
 		System.out.println("The Array with match pair is : " + Matchpair(arr, target));
 		System.out.println();
+
+		System.out.println("The Array with match pair is : ");
+		unigCharHm(arr, target);
+
 	}
 
+	// //method to display match pair using Arraylist
 	private static ArrayList<String> Matchpair(int[] array, int target) {
 
 		ArrayList<String> new_arrlist = new ArrayList<String>();
@@ -32,13 +38,39 @@ public class MatchPair {
 				if (array[i] + array[j] == target) {
 					int x = array[i];
 					int y = array[j];
-
-					new_arrlist.add("{" + x + "," + y + "}");
+					if (!new_arrlist.contains(array[i]) || !new_arrlist.contains(array[j])) {
+						new_arrlist.add("{" + x + "," + y + "}");
+					}
 
 				}
 			}
 		}
 		return new_arrlist;
+	}
+
+	// method to display match pair using hashmap
+	public static void unigCharHm(int[] arr, int target) {
+
+		HashMap<Integer, Boolean> Newhm = new HashMap<Integer, Boolean>();
+		// TODO Auto-generated method stub
+		for (int j = 0; j < arr.length; j++) {
+			int x = target - arr[j];
+			int y = arr[j];
+
+			if (Newhm.containsKey(x) && Newhm.get(x) == false) {
+
+				System.out.println("{" + y + "," + x + "}");
+
+				Newhm.put(y, true);
+				Newhm.put(x, true);
+			}
+
+			else {
+			  	Newhm.put(y, false);
+			}
+
+		}
+
 	}
 
 }
